6e5a841e374d7a532137542584fd378939e27a52
Tim Carlsson <timcarlsson0203@gmail.com>
2025-11-03 11:13:23 +0100

    add

--- lua/cynosure/palettes/cynosure.lua
+++ lua/cynosure/palettes/cynosure.lua
@@ -21,7 +21,7 @@ return {
   raw_string = "#B2D89D",
   cursor = "#5DCD9A",
   operator = "#b09884",
-  constant = "#BCB6EC",
+  constant = "#408FAF",
   keyword = "#9C7514",
   important = "#408FAF",
   search = "#E9B872",
